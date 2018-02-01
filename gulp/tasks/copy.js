c78314e6295f6eac748bd8c1cd988bcecb12b3a3
Sergey Khmelevskoy <xs290@me.com>
2018-02-01 19:15:23 +0300

    pace preloader

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -33,6 +33,7 @@ gulp.task('copy', [
 ]);
 
 gulp.task('copy:watch', function() {
+  gulp.watch(config.src.root + '/*.*', ['copy:rootfiles']);
   gulp.watch(config.src.vendor + '/**/*.*', ['copy:vendor']);
   gulp.watch(config.src.root + '/video/**.*', ['copy:video']);
   gulp.watch(config.src.fonts + '/*.{ttf,eot,woff,woff2}', ['copy:fonts']);
